c2a7aca344888e0841aa0be54223766874e65f60
Casey Zwicker <caseypzwicker@gmail.com>
2025-02-14 13:27:44 -0800

    so far...

--- Tempo-Blade.ino
+++ Tempo-Blade.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include "notes.h"
 
 const int rs = 7;
 const int e  = 8;
@@ -9,6 +10,11 @@ const int d7 = 12;
 
 const int columns = 16;
 const int rows = 2;
+
+#define C3  131
+#define Db3 139
+#define D3  147
+#define D3  147
 
 byte uarrow[8] = {
   0b00000,
@@ -121,18 +127,70 @@ void setup() {
 
 // good wrong buzzer sound: 4ms high, 4ms low
 void loop() {
-  static long int lastMicros = 0;
-  int pitch = 5000;
+  /*
+  static int pitches[] = {Gb5,Gb5,Gb5,Gb5,D5 ,D5 ,D5 ,D5 ,
+                          B4 ,B4 ,B4 ,B4 ,G4 ,G4 ,G4 ,Bb4,
+                          Bb4,Bb4,Bb4,Bb4,0  ,0  ,0  ,0  ,
+                          B4 ,B4 ,B4 ,A4 ,A4 ,A4 ,A4 ,A4 ,
+                          D5 ,D5 ,D5 ,D5 ,Bb4,Bb4,Bb4,Bb4,
+                          G4 ,G4 ,G4 ,G4 ,Eb4,Eb4,Eb4,Gb4,
+                          Gb4,Gb4,Gb4,Gb4,0  ,0  ,0  ,0  ,
+                          G4 ,G4 ,G4 ,G4 ,F4 ,F4 ,F4 ,Bb4,
+                          Bb4,Bb4,Bb4,Bb4,0  ,0  ,0  ,0  ,
+                          B4 ,B4 ,B4 ,B4 ,A4 ,A4 ,A4 ,D5 ,
+                          D5 ,D5 ,D5 ,D5 ,0  ,0  ,0  ,0  ,
+                          Eb5,Eb5,Eb5,0  ,Eb5,Eb5,Eb5,Gb5,
+                          Gb5,Gb5,Gb5,Gb5,Gb5,Gb5,Gb5,0  ,
+                          G5 ,G5 ,G5 ,0  ,G5 ,G5 ,G5 ,Bb5,
+                          Bb5,Bb5,Bb5,Bb5,Bb5,Bb5,Bb5,0  ,
+                          Gb5,Gb5,0  ,Gb5,0  ,0  ,0  ,0  ,
 
-  // put your main code here, to run repeatedly:
-  if((micros() - lastMicros) % (1000000/pitch) < 500000/pitch) {
+                          Gb5,Gb5,Gb5,Gb5,D5 ,D5 ,D5 ,D5 ,
+                          B4 ,B4 ,B4 ,B4 ,G4 ,G4 ,G4 ,Bb4,
+                          Bb4,Bb4,Bb4,Bb4,0  ,0  ,0  ,0  ,
+                          B4 ,B4 ,B4 ,A4 ,A4 ,A4 ,A4 ,A4 ,
+                          D5 ,D5 ,D5 ,D5 ,Bb4,Bb4,Bb4,Bb4,
+                          G4 ,G4 ,G4 ,G4 ,Eb4,Eb4,Eb4,Gb4,
+                          Gb4,Gb4,Gb4,Gb4,0  ,0  ,0  ,0  ,
+                          G4 ,G4 ,G4 ,G4 ,F4 ,F4 ,F4 ,Bb4,
+                          Bb4,Bb4,Bb4,Bb4,0  ,0  ,0  ,0  ,
+                          B4 ,B4 ,B4 ,B4 ,A4 ,A4 ,A4 ,D5 ,
+                          D5 ,D5 ,D5 ,D5 ,0  ,0  ,0  ,0  ,
+                          Eb5,Eb5,Eb5,0  ,Eb5,Eb5,Eb5,Gb5,
+                          Gb5,Gb5,Gb5,Gb5,Gb5,Gb5,Gb5,0  ,
+                          G5 ,G5 ,G5 ,0  ,G5 ,G5 ,G5 ,Bb5,
+                          Bb5,Bb5,Bb5,Bb5,Bb5,Bb5,Bb5,0  ,
+                          Db5,E5 ,Ab5,B5 ,Bb5,Bb5,Bb5,Ab5,
+                          
+                          Gb5,};
+                          */
+
+  static int pitches[] = {A4,A4,A4,A4,A4,A4,A4,A4,A4,A4,A4,A4,A4,};
+  static int pitch;
+
+
+  if(millis() < sizeof(pitches)/sizeof(int) * 91) {
+    pitch = pitches[millis() / 91];
+  } else {
+    pitch = 0;
+  }
+
+
+
+
+
+
+
+  if(pitch == 0) {
+    digitalWrite(4, LOW);
+    digitalWrite(3, LOW);
+    return;
+  }
+  if(micros() % (1000000/pitch) < 500000/pitch) {
     digitalWrite(4, HIGH);
     digitalWrite(3, LOW);
   } else {
     digitalWrite(4, LOW);
     digitalWrite(3, HIGH);
-  }
-  if(micros() - lastMicros == 1000000/pitch) {
-    lastMicros = micros();
   }
 }
