4de95d88da60fc4943f1e039dce6ae4bb957b75d
khoulihan27 <khoulihan@ucsb.edu>
2025-02-14 12:19:39 -0800

    json parser --> test.py

---
+++ test.py
@@ -0,0 +1,50 @@
+import json
+import os
+
+class Note: 
+    def __init__(self, time, color, direction):
+        self.time = time
+        self.color = color
+        self.direction = direction
+
+    def getTime(self):
+        return self.time
+
+    def getColor(self):
+        return self.color
+
+    def getDirection(self):
+        return self.direction
+
+
+cwd = os.getcwd()
+fileName = None
+fileParsed = False
+
+for root, dirs, files in os.walk(cwd):
+    for name in files:
+        if name.endswith((".dat")):
+            fileName = os.path.join(cwd, name)
+            print(fileName)
+
+if fileName != None:
+    with open(fileName) as data:
+        parsedFile = json.load(data)
+        print("parsed the file!")
+        fileParsed = True
+else:
+    print("file not found!")
+
+if fileParsed == True:
+    notesList = []
+    direction = 0
+
+    for url in parsedFile["_notes"]:
+        direction = url["_cutDirection"]
+        if direction >= 4:
+            direction = direction - 4 - (direction // 8)
+        notesList.append(Note(url["_time"], url["_type"], direction))
+
+    for doofus in notesList:
+        print("Time: ", doofus.getTime(), ", Color: ", doofus.getColor(), ", Direction: ", doofus.getDirection(), "\n")
+
